7db0d94ff9845421bd01dedfef4ac7c8b2dd75e8
n.b.q.x <nbqxnbq@gmail.com>
2014-04-21 16:57:57 +0900

    fix manifest.js

--- resources/manifest.js
+++ resources/manifest.js
@@ -1,9 +1,11 @@
 {
   header: ["#target InDesign-7.0"],
   src: ["tpl.jsx"],
-  preProcess: function(body,opt){
-    var content = "var DATA = "+JSON.stringify(opt)+";";
-    body = body.replace(/(\/\* @options\{\{ \*\/)(.|\n|\r|\u2028|\u2029)+(\/\* \}\}@options \*\/)/g, content);
-    return body
+  preProcess: {
+    "tpl.jsx": function(body,opt){
+      var content = "var DATA = "+JSON.stringify(opt)+";";
+      body = body.replace(/(\/\* @options\{\{ \*\/)(.|\n|\r|\u2028|\u2029)+(\/\* \}\}@options \*\/)/g, content);
+      return body
+    }
   }
 }
